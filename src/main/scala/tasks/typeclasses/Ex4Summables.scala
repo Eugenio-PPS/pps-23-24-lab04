c3612d2acf95a5737936e7ed041fb60a9a75c595
Eugenio Tampieri <eugenio.tampieri@studio.unibo.it>
2024-03-25 11:24:52 +0100

    [ex2] done

--- src/main/scala/tasks/typeclasses/Ex4Summables.scala
+++ src/main/scala/tasks/typeclasses/Ex4Summables.scala
@@ -18,14 +18,26 @@ object Ex4Summables:
     def sum(a1: A, a2: A): A
     def zero: A
 
-  def sumAll[A: Summable](seq: Sequence[A]) = 
+  def sumAll[A: Summable](seq: Sequence[A]): A =
     val summable = summon[Summable[A]]
-    ???  // complete here
+    seq match
+      case Sequence.Cons(head, tail) => summable.sum(head, sumAll[A](tail))
+      case Sequence.Nil() => summable.zero
 
   given Summable[Int] with
     def sum(a1: Int, a2: Int): Int = a1 + a2
     def zero: Int = 0
-  
+
+  given Summable[Double] with
+    def sum(a1: Double, a2: Double): Double = a1 + a2
+
+    def zero: Double = 0
+
+  given Summable[String] with
+    def sum(a1: String, a2: String): String = a1 + a2
+
+    def zero: String = ""
+
   // write givens for Summable[Double] and Summable[String]
 
   @main def trySummables =
@@ -33,7 +45,7 @@ object Ex4Summables:
     println:
       sumAllInt(si) // 60
 
-    /* uncomment from here   
+    /* uncomment from here   */
 
     println:
       sumAll(si) // 60
@@ -46,5 +58,5 @@ object Ex4Summables:
     println:
       sumAll(ss) // "102030"
 
-    */  
+    /**/
 
